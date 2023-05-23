ba932118130aa34601e0c35d9718d9762365db2c
mkhajiit <132371050+mkhajiit@users.noreply.github.com>
2023-05-23 20:22:35 +0900

    Add files via upload

---
+++ selfPrac01.js
@@ -0,0 +1,8 @@
+// const DIV=document.querySelector("div");
+// const TEXT = DIV.innerText+"\u00a0"; //"\u00a0"공백
+// DIV.style.color="red";
+// DIV.style.textAlign="left";
+// DIV.style.fontVariant="small-caps";
+// DIV.style.fontFamily="serif";
+
+// document.write(TEXT.repeat(5));
